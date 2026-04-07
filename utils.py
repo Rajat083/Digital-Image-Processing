fc10a41676000571bd187d83d91ab9d192eb8e5d
Rajat083 <155555359+Rajat083@users.noreply.github.com>
2026-04-07 14:15:58 +0530

    Lab 08

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def compute_histogram(img):
     hist = np.zeros(256, dtype=int)
@@ -27,3 +28,24 @@ def histogram_equalization(img):
             equalized[i, j] = mapping[img[i, j]]
 
     return equalized, mapping
+
+
+def apply_filter(image, kernel):
+    kh, kw = kernel.shape
+    h, w, c = image.shape
+    pad_h, pad_w = kh // 2, kw // 2
+    padded_img = np.pad(image, ((pad_h, pad_h), (pad_w, pad_w), (0, 0)), mode='edge')
+    
+    res_img = np.zeros_like(image, dtype=np.float32)
+    for i in range(h):
+        for j in range(w):
+            region = padded_img[i : i + kh, j : j + kw, :]
+            res_img[i, j, :] = np.sum(region * kernel[:, :, np.newaxis], axis=(0, 1))
+            
+    return res_img
+
+def print_image(image):
+    plt.imshow(image, cmap='gray')
+    plt.title(f'Image Shape: {image.shape}')
+    plt.axis('off')
+    plt.show()
